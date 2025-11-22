15d811344f9254100a4e867c3a668b4980e8f84b
Ramkrishna Sharma <philosophicalbuilder@gmail.com>
2025-11-21 19:26:10 -0500

    Update PA4solution.java

--- PA4solution.java
+++ PA4solution.java
@@ -18,8 +18,23 @@ public class PA4solution {
      * 
      * I'm pasting my from 2023 to Kadane's algorithim here for reference
      * 
+     * * class Solution {
+     * public int maxSubArray(int[] nums) {
+     * // Initialize our variables using the first element.
+     * int currentSubarray = nums[0];
+     * int maxSubarray = nums[0];
      * 
+     * // Start with the 2nd element since we already used the first one.
+     * for (int i = 1; i < nums.length; i++) {
+     * int num = nums[i];
+     * // If current_subarray is negative, throw it away. Otherwise, keep adding to
+     * it.
+     * currentSubarray = Math.max(num, currentSubarray + num);
+     * maxSubarray = Math.max(maxSubarray, currentSubarray);
+     * }
      * 
+     * return maxSubarray;
+     * }
      * 
      * 
      * 
