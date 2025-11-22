c536b6cec7967de977e29a81a7b3bdcd41fb1509
Ramkrishna Sharma <philosophicalbuilder@gmail.com>
2025-11-21 19:25:15 -0500

    Update PA4solution.java

--- PA4solution.java
+++ PA4solution.java
@@ -1,3 +1,28 @@
 public class PA4solution {
-    // initial push
+    /*
+     * 
+     * At first, to me, this looks like a sliding window problem. However, on second
+     * inspection,
+     * it seems to be a Kadane's algorithim problem borrowing from solution to the
+     * Maximum Subarray Sum problem
+     * which I solved in 2023:
+     * https://drive.google.com/file/d/1I3uP0MD2pY6kpmCo0NsjwkOTScndegu3/view?usp=
+     * share_link
+     * 
+     * I initially thought this was a sliding window approach similar to my video:
+     * Longest Consecutive Subsequence: https://www.youtube.com/watch?v=GmlMx5IMEqE
+     * However, this problem accumlates (or should accumlate) an ongoing score (or
+     * sum) based on encountered letters. And, find the goal seems to be to find the
+     * window where that score difference is maximized.
+     * 
+     * 
+     * I'm pasting my from 2023 to Kadane's algorithim here for reference
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
 }
